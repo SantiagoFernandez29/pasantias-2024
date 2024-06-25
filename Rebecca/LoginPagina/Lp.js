b87234d391ab4b381be48e2439917887e685a129
Becca-Ro <angel.ho.rodriguez@gmail.com>
2024-06-25 11:27:56 -0300

    loginPage

---
+++ Rebecca/LoginPagina/Lp.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+document
+  .querySelector(".button-toggle")
+  .addEventListener("click", toggleInputType);
+
+function toggleInputType(params) {
+  
+  const InputElement = document.querySelector(".input-toggle");
+  if (InputElement.type === "password"){
+    InputElement.type = "text";
+  }
+  else {
+    InputElement.type = "password"; 
+  }
+
+  console.log(document.querySelector('.button-toggle'));
+
+
+  const showIcon = document
+    .querySelector(".showIcon")
+    .classList.toggle("hidden");
+  const hideIcon = document
+    .querySelector(".hideIcon")
+    .classList.toggle("show");
+}
+});
